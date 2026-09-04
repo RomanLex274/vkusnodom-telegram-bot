1c5bacebb5c0a14c28d226325c233ca91236e154
RomanLex274 <tysimmm@yandex.ru>
2026-09-04 12:25:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ TELEGRAM_CHANNEL = os.getenv('TELEGRAM_CHANNEL')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 RSS_FEEDS = [
     'https://eda.ru/rss',
